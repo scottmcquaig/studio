5170c06d33e960a8d4e7b9806748c1faaeb290bc
Scott McQuaig <scottmcquaig@gmail.com>
2025-08-14 03:59:49 +0000

    change badge font to Inter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
+        sans: ['Inter', 'sans-serif'],
         body: ['Open Sans', 'sans-serif'],
         headline: ['Roboto', 'sans-serif'],
         code: ['monospace'],
